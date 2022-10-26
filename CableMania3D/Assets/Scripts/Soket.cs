9ba7096229bd74104d19d11d533738db5f8b55ac
bahauslusoy <105641542+bahauslusoy@users.noreply.github.com>
2022-10-26 09:59:39 +0300

    Add : Models , Scripts , Environmental

---
+++ CableMania3D/Assets/Scripts/Soket.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soket : MonoBehaviour
+{
+    [SerializeField] private bool isFully;
+    [SerializeField] private GameObject movePos ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
